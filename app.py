3ed4f7b3706dd35cccfd690ebef96124d0ea290a
jennylei1011-coder <jenny.lei1011@gmail.com>
2026-05-11 04:51:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-st.title("🍬 JENNY对账机器人 · 甜蜜版")
+st.title("🍬 JENNY对账机器人 ")
 st.markdown("""
 <div style="background:#FFE4E1; padding:15px; border-radius:20px; margin-bottom:20px;">
 🌸 <b>使用流程：</b><br>
@@ -647,7 +647,7 @@ if st.button("✨ 开始自动对账", type="primary"):
                 sys_dup.to_excel(writer, sheet_name="5.系统重复", index=False)
                 jnl_dup.to_excel(writer, sheet_name="6.日记账重复", index=False)
 
-            st.success("🎉 对账完成！请下载甜蜜报告～")
+            st.success("🎉 对账完成！请下载报告～")
             st.download_button(
                 label="📥 下载对账报告",
                 data=output.getvalue(),
